3dc384c75a2769547e914c0ef867150ddc121b03
Brandon Lowery <bdl3k@mtmail.mtsu.edu>
2018-01-27 03:22:39 -0600

    Add files via upload
    reference file for mysql & python

---
+++ test_server_connection.py
@@ -0,0 +1,34 @@
+import mysql.connector
+from mysql.connector.cursor import MySQLCursorPrepared #new
+
+cnx = mysql.connector.connect(user='brandon', password='lowery',
+                              host='104.198.251.11',
+                              database='data')
+
+
+#cursor = cnx.cursor()
+cursor = cnx.cursor(cursor_class=MySQLCursorPrepared) #new
+
+cursor = cnx.cursor(prepared=True) #new
+cursor = cnx.cursor(buffered=True)
+
+k = 17
+x = "UPDATE LOT_INFO SET car_count = (%s) WHERE lot_name = (%s)"
+
+cursor.execute(x, (k,"kom"))
+
+query = ("SELECT car_count FROM LOT_INFO WHERE lot_name = %s")
+
+cursor.execute(query, ("kom",))
+
+#print(query)
+
+
+
+for row in cursor:
+    print(row)
+
+
+
+cursor.close()
+cnx.close()
