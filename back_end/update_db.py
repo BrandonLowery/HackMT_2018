7d65c992cd308904afc5c004000a4cceb929c6da
brandonlowery <bdl3k@mtmail.mtsu.edu>
2018-01-27 17:45:48 -0600

    updated code partially working

--- back_end/update_db.py
+++ back_end/update_db.py
@@ -3,20 +3,53 @@
 
 import mysql.connector
 from mysql.connector.cursor import MySQLCursorPrepared #new
+cnx = mysql.connector.connect(user='brandon', password='lowery',
+                              host='104.198.251.11',
+                              database='data')
 
 
+class database_update():
 
-class  idk(object):
-
-    cnx = mysql.connector.connect(user='brandon', password='lowery',
-                                  host='104.198.251.11',
-                                  database='data')
+    def __init__(self, name):
+        self.car_count = 0
+        self.id = name
 
 
-    def add_one(string):
+    def get_count(self):
         cursor = cnx.cursor(cursor_class=MySQLCursorPrepared) #new
 
         cursor = cnx.cursor(prepared=True) #new
         cursor = cnx.cursor(buffered=True)
+        query = ("SELECT car_count FROM LOT_INFO WHERE lot_name = %s")
 
-        
+        #cursor.execute(query, ("kom",))
+        cursor.execute(query, (self.id,))
+        #return self.car_count
+        for row in cursor:
+            count = row
+
+        cursor.close()
+        cnx.close()
+
+
+        return count
+
+    def get_id(self):
+        return self.id
+
+    def add(self):
+        self.car_count += 1
+
+    def sub(self):
+        self.car_count -= 1
+
+kom = database_update("kom") #creates a db_update object named kom
+
+
+kom.add() #adds one to the count in the db for kom
+
+x = kom.get_count() #gives you kom lot count
+
+print(x)
+k = kom.get_id()
+print(k)
